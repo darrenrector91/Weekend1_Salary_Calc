e9a12c6e2a9de3d9d21bbf7c5e7b5f3b5f69aea1
Darren Rector <darrenrector91@gmail.com>
2017-12-15 17:28:56 -0600

    create table with basic styling

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(salaryCalculator);
+
+function salaryCalculator() {
+  //event listener
+
+});
+
+// create input form(table) including:
+//_employee first name, last name,
+// ID number, job title, annual salary_
+
+// create submit button
+
+// create function to to store the information to calculate monthly costs
+
+// append information to DOM(table)
+
+//clear input fields
+
+//use stored data to calculate monthly costs
+
+//append to the DOM(table)
+
+// *********HARD mode***************
+
+// create delete button
+
+// use delete button to remove an employee from DOM
+
+//style page
