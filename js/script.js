365b3e4556e246976a47549ecf779b7134601a6a
Darren Rector <darrenrector91@gmail.com>
2017-12-15 23:02:08 -0600

    added focus to first name input/cleared inputs on click

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
   console.log('DOM loaded');
 
-  $(".addButton").click(function() {
-    var first = $("#firstname").val();
-    var last = $("#lastname").val();
-    var job = $("#jobtitle").val();
-    var annual = $("#annualsalary").val();
-    var data = "<tr><td><input type='checkbox' name='chkRemove'></td><td>" + first + "</td><td>" + last + "</td><td>" + job + "</td><td>" + annual + "</td></tr>";
-    $("table tbody").append(data);
-  });
+  // event handlers
 
+
+  $(".addButton").click(function() {
+    if ($("#firstname").val().length > 0) {
+      var first = $("#firstname").val();
+      var last = $("#lastname").val();
+      var job = $("#jobtitle").val();
+      var annual = $("#annualsalary").val();
+      var data = "<tr><td><input type='checkbox' name='chkRemove'></td><td>"
+      + first +
+      "</td><td>"
+      + last +
+      "</td><td>"
+      + job +
+      "</td><td>"
+      + annual +
+      "</td></tr>";
+
+      $("table tbody").append(data);
+
+      $("#firstname").val('').focus();
+      $("#lastname").val('');
+      $("#jobtitle").val('');
+      $("#annualsalary").val('');
+    } else {
+      alert('Please add employee');
+    }
+  });
+  //$('#guess_input').val('').focus(); //clear input
   // Find and remove selected table rows
   $("#deleteRow").click(function() {
     $("table tbody").find('input[name="chkRemove"]').each(function() {
