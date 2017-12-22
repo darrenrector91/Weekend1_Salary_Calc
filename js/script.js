7f23e2a44316f3610a64484f575cf658a4d3eac8
Darren Rector <darrenrector91@gmail.com>
2017-12-22 11:03:10 -0600

    Weekend challenge submission

--- js/script.js
+++ js/script.js
@@ -1,45 +1,46 @@
 $(document).ready(function() {
-  console.log('DOM loaded');
-
   // event handlers
+  $(".addButton").on('click', addEmp)
+  $('#salaryBody').on('click', '.delete', deleteEmp);
 
-  $(".addButton").click(function() {
+  function addEmp() {
+    counter = 0;
     if ($("#firstname").val().length > 0) {
+      var id = $("#id").val();
       var first = $("#firstname").val();
       var last = $("#lastname").val();
       var job = $("#jobtitle").val();
       var annual = $("#annualsalary").val();
-      var monthly = annual/12;
+      var monthly = annual / 12;
       console.log(monthly);
-      var data = "<tr><td><input type='checkbox' name='chkRemove'></td><td>"
-      + first +
-      "</td><td>"
-      + last +
-      "</td><td>"
-      + job +
-      "</td><td>"
-      + annual +
-      "</td><td>"
-      + monthly +
-      "</td></tr>";
-
+      var data = "<tr><td>" +
+        id +
+        "</td><td>" +
+        first +
+        "</td><td>" +
+        last +
+        "</td><td>" +
+        job +
+        "</td><td>" +
+        annual +
+        "</td><td>" +
+        monthly +
+        "</td><td><button class='delete'>Delete</button></td></tr>";
       $("table tbody").append(data);
 
-      $("#firstname").val('').focus();
+      $("#annualExpense").append(monthly);
+
+      $("#id").val('').focus();
+      $("#firstname").val('');
       $("#lastname").val('');
       $("#jobtitle").val('');
       $("#annualsalary").val('');
     } else {
-      alert('Please add employee');
+      alert('Please add employee information');
     }
-  });
-  //$('#guess_input').val('').focus(); //clear input
-  // Find and remove selected table rows
-  $("#deleteRow").click(function() {
-    $("table tbody").find('input[name="chkRemove"]').each(function() {
-      if ($(this).is(":checked")) {
-        $(this).parents("tr").remove();
-      }
-    });
-  });
+  };
+  // Find and remove table rows
+  function deleteEmp() {
+    $(this).parent().parent().remove();
+  }
 });
